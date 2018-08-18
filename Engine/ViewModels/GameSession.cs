32d31f27e97bfd8c5483c518646b61660223e76d
Robert Hinds <hindsr@uw.edu>
2018-08-17 21:19:42 -0700

    connected wpfui to player model via the gamesesion (viewmodel). UI now displays player information. Gamesession creates a new Player object and assigns values to its property.

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 using Engine.Models;
 namespace Engine.ViewModels
 {
-    class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Jack";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
             CurrentPlayer.Gold = 1000000;
 
         }
--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -12,17 +12,22 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Engine.ViewModels;
 
-namespace UI
+namespace WPFUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GameSession _gameSession;
         public MainWindow()
         {
             InitializeComponent();
+            _gameSession = new GameSession();
+
+            DataContext = _gameSession;
         }
     }
 }
