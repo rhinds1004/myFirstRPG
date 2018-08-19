7cd804b9af64e5b4f855189d321e4fff13737863
Robert Hinds <hindsr@uw.edu>
2018-08-18 21:45:47 -0700

    Added location class. Holds informations about a location in the game. XY corrds, name, descp and Image. Created instance of location object called home in Gamession class. MainWindow xaml was modified with more rows and columns. Also now uses databinding to get the location information and display it.

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -9,7 +9,7 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
-
+        public Locations CurrentLocation { get; set; }
         public GameSession()
         {
             CurrentPlayer = new Player();
@@ -20,6 +20,12 @@ namespace Engine.ViewModels
             CurrentPlayer.Level = 1;
             CurrentPlayer.Gold = 1000000;
 
+            CurrentLocation = new Locations();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
         }
     }
 }
