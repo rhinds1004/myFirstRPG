d1e6abec64dc85d237ec515b17a9c6fd74e9ea03
rhinds1004 <rhinds1004@gmail.com>
2018-08-22 19:13:10 -0700

    Added QuestFactory. used Linq as the way to search for a quest. Used Linq's firstOrDestory finds first reference to the id or returns default item which in quest class's case is null. Linq is intermixed with List<> Added itemquantity class that can be used to whole the information for items to finish quest and the rewards for finishing a quest.  Maybe reuse ItemQuantity for whole items in player inventory?

---
+++ Engine/Factories/QuestFactory.cs
@@ -0,0 +1,38 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Factories
+{
+    internal static class QuestFactory
+    {
+        private static readonly List<Quest> _quests = new List<Quest>();
+
+        static QuestFactory()
+        {
+            //Declare the items need to complete the quest, and its reward items
+            List<ItemQuantity> itemsToComplete = new List<ItemQuantity>();
+            List<ItemQuantity> rewardItems = new List<ItemQuantity>();
+
+            itemsToComplete.Add(new ItemQuantity(9001, 5));
+            rewardItems.Add(new ItemQuantity(1002, 1));
+
+            //Create the quest
+            //TODO make the location name generic so can be used in other locations??
+            _quests.Add(new Models.Quest(1,
+                                         "Clear the herb garden!",
+                                         "Defeat the snakes in the Herbalist's garden",
+                                         itemsToComplete,
+                                         25, 10,
+                                         rewardItems));
+        }
+
+        internal static Quest GetQuestByID(int id)
+        {
+            return _quests.FirstOrDefault(quest => quest.ID == id);
+        }
+    }
+}
