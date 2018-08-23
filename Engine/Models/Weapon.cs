db303ab1a579fed08609a72a5c94175d78cc874d
rhinds1004 <rhinds1004@gmail.com>
2018-08-22 17:33:34 -0700

    Created Itemfactory class. Since it static can not have a constructor, but when when Itemfactory is first called it will create a list and fill the list with items.  Item factory has function that creates a game item, but instead of just return  the Gameitem found in the list it copies the via the clone function found in gameitem and weapon class. THis allows many items of the same type eg. "pointy stick" to exist in the gameworld. Which will allow us to modify a given gameitem. The Gameitem class is the base item class that holds the basic information/functions shared by all items. Weapon class inherits from gameitem and adds two additional properties and overwrites the gameitem clone function to include the additional weapon only properties.  NOTE: since Weapon's clone function overrode Gameitem's had to use this sytax  public new Weapon Clone().  The use of cloning an object appears to be the prototype design pattern.

---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public int MinimumDamge { get; set; }
+        public int MaximumDamge {get; set; }
+
+        public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage)
+            : base(itemTypeID, name, price)
+        {
+            MinimumDamge = minDamage;
+            MaximumDamge = maxDamage;
+        }
+
+        public new Weapon Clone()
+        {
+            return new Weapon(ItemTypeID, Name, Price, MinimumDamge, MaximumDamge);
+        }
+    }
+}
