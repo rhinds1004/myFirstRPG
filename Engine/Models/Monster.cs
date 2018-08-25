96089aa04db0dda7ebf8019629293c75ea3c55b8
rhinds1004 <rhinds1004@gmail.com>
2018-08-25 16:31:42 -0700

    Added a commet as to way why the creation of the inventory list was handled differently compared to the other variables.

--- Engine/Models/Monster.cs
+++ Engine/Models/Monster.cs
@@ -5,6 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Note: from the website. Inventory was passed in as a parameter as the author feels
+ * this can lead to new temporary lists being created that have a short life.
+ * Author likes to instaiate a new list variable in the constructor then wrap the add function
+ * within AddItem function that can then have validation code before actually adding an item to
+ * a list. Example code of issue:  https://gist.github.com/ScottLilly/717fba395154c445237011058c809c5b
+ */ 
 namespace Engine.Models
 {
     public class Monster : BaseNotificationClass
